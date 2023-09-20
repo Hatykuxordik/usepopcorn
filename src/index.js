bc0ae52d54acd14702ee7cbeae80bd8bde4de7f8
hatykuxordik <hatykuxordik@gmail.com>
2023-09-20 15:50:04 +0100

    Update Watched movies

--- src/index.js
+++ src/index.js
@@ -4,24 +4,9 @@ import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
 
-// function Test() {
-//   const [movieRating, setMovieRating] = useState(0);
-//   return (
-//     <div>
-//       <StarRating color="blue" maxRating={8} onSetRating={setMovieRating} />
-//       <p>The movie was rated {movieRating} star</p>
-//     </div>
-//   );
-// }
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating maxLength={7} />
-    <StarRating maxRating={10} color="red" size={32} />
-    <StarRating size={128} setDefault={4} mo />
-
-    <Test /> */}
   </React.StrictMode>
 );
