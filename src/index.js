48da1dd5ef9ce141eddca00fbfa7e5e1e9301a28
hatykuxordik <hatykuxordik@gmail.com>
2023-10-07 11:09:42 +0100

    Complete Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -7,5 +7,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
   </React.StrictMode>
 );
